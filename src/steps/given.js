0d1ad4d885e5e749d15d0e4b54d61d05351d0b09
Cassity Martin <cassity.martin@lilly.com>
2024-05-08 16:14:32 -0400

    added given steps

--- src/steps/given.js
+++ src/steps/given.js
@@ -2,10 +2,16 @@ const { Given } = require("@wdio/cucumber-framework");
 
 const LoginPage = require("../pageobjects/login.page");
 const InventoryPage = require("../pageobjects/inventory.page");
+const customerdataPage = require("../pageobjects/customerdata.page");
+const checkoutPage = require("../pageobjects/checkout.page");
+const completePage = require("../pageobjects/complete.page");
 
 const pages = {
   login: LoginPage,
   inventory: InventoryPage,
+  customerdataPage: customerdataPage,
+  checkoutPage: checkoutPage,
+  completePage: completePage,
 };
 
 Given(/^I am on the (\w+) page$/, async (page) => {
